e5ecfa8bc86ef52e48f05281b7dc9510144f3888
Rene Balke <renebalke2@gmail.com>
2018-12-10 16:57:53 +0100

    implemented workaround for nav drawer/table view bug

--- app/src/main/java/de/bmtrading/stockfundamentals/MainActivity.kt
+++ app/src/main/java/de/bmtrading/stockfundamentals/MainActivity.kt
@@ -1,20 +1,21 @@
 package de.bmtrading.stockfundamentals
 
-import android.annotation.TargetApi
-import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.NavigationView
 import android.support.v4.widget.DrawerLayout
 import android.view.View
+import android.widget.Toast
 import de.bmtrading.stockfundamentals.keyfigures.KeyFiguresFragment
 import de.bmtrading.stockfundamentals.overview.OverviewFragment
 import iex.IexApiController
+import java.time.LocalTime
 
 class MainActivity : AppCompatActivity() {
-    lateinit var mDrawerLayout:DrawerLayout
+    private var mBackPressedTimestamp: Long = 0
+    private lateinit var mDrawerLayout:DrawerLayout
 
-            companion object {
+    companion object {
         @JvmStatic
         val mIexApiController: IexApiController = IexApiController()
     }
@@ -36,44 +37,22 @@ class MainActivity : AppCompatActivity() {
 
             if(menuItem.itemId == R.id.nav_key_figures){
                 supportFragmentManager.beginTransaction().add(R.id.content_frame, KeyFiguresFragment(), KeyFiguresFragment::class.java.simpleName).commit()
+                mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
             }
             true
         }
-       // hideNavigationBar()
     }
 
-    @TargetApi(19)
-    private fun hideNavigationBar() {
-        val flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View
-                .SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN or View
-                .SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-
-        // This work only for android 4.4+
-        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-
-            window.decorView.systemUiVisibility = flags
-
-            // Code below is to handle presses of Volume up or Volume down.
-            // Without this, after pressing volume buttons, the navigation bar will
-            // show up and won't hide
-            val decorView = window.decorView
-            decorView.setOnSystemUiVisibilityChangeListener { visibility ->
-                if (visibility and View.SYSTEM_UI_FLAG_FULLSCREEN == 0) {
-                    decorView.systemUiVisibility = flags
-                }
-            }
+    override fun onBackPressed(){
+        //if fraement visible go back to main screen
+        val frag = supportFragmentManager.findFragmentByTag(KeyFiguresFragment::class.java.simpleName)
+        if(frag != null && frag.isVisible) {
+            frag.view?.visibility = View.GONE
+            mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+        }else{
+            if (System.currentTimeMillis() < mBackPressedTimestamp + 2000) System.exit(0)
+            mBackPressedTimestamp = System.currentTimeMillis()
+            Toast.makeText(this, getString(R.string.sure_exit), Toast.LENGTH_SHORT).show()
         }
     }
-/*
-    override fun onWindowFocusChanged(hasFocus: Boolean) {
-        super.onWindowFocusChanged(hasFocus)
-        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && hasFocus) {
-            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or View
-                    .SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
-                    View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-        }
-    }
-    */
 }
